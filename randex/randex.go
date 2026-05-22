73f3ebd95fd890f70e904f15360cf84568fb8eac
Igor Karatayev <igor.cwer@gmail.com>
2026-05-22 13:18:17 +0300

    AngleFloat randoms in range 0; 2*pi

--- randex/randex.go
+++ randex/randex.go
@@ -1,6 +1,7 @@
 package randex
 
 import (
+	"math"
 	"math/rand/v2"
 	"time"
 )
@@ -13,4 +14,12 @@ func NewTimePCGRand() *rand.Rand {
 	randSource.Seed(_rand.Uint64(), _rand.Uint64())
 
 	return _rand
+}
+
+func AngleFloat32() float32 {
+	return 2 * math.Pi * rand.Float32()
+}
+
+func AngleFloat64() float64 {
+	return 2 * math.Pi * rand.Float64()
 }
