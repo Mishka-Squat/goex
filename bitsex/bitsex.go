bb6112c671a44327eb48d5e8c1d49a8852ee2c71
Igor Karatayev <igor.cwer@gmail.com>
2026-07-18 04:33:42 +0300

    wip system to task conversion - mostly works, input is broken after colony window

--- bitsex/bitsex.go
+++ bitsex/bitsex.go
@@ -5,7 +5,11 @@ import "math/bits"
 const uintSize = 32 << (^uint(0) >> 63)
 
 func MaskLeft(width uint) uint {
-	return ((1<<width + 1) - 1) << (uintSize - width)
+	return (1<<(width+1) - 1) << (uintSize - width)
+}
+
+func MaskRight(width uint) uint {
+	return (1<<(width+1) - 1)
 }
 
 func HaveIncodedMask(n uint) int {
