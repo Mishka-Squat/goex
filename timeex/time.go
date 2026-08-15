bacfb0a1143c9f4d768107754cc7d277fd49118d
Igor Karatayev <igor.cwer@gmail.com>
2026-08-15 23:46:41 +0300

    wip animations

--- timeex/time.go
+++ timeex/time.go
@@ -5,3 +5,7 @@ import "time"
 const unixToInternal = int64((1969*365 + 1969/4 - 1969/100 + 1969/400) * 24 * 60 * 60)
 
 var EndOfTime = time.Unix(1<<63-1-unixToInternal, 999999999)
+
+func Float32(dt time.Duration) float32 {
+	return float32(dt) / float32(time.Second)
+}
